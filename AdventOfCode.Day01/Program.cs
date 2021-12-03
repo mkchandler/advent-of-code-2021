888b466c3ab4a37c7b652c181303267744688b80
Matt Chandler <mkchandler@gmail.com>
2021-12-02 22:42:21 -0600

    Added solutions for days 1 and 2

---
+++ AdventOfCode.Day01/Program.cs
@@ -0,0 +1,59 @@
+﻿// Part One
+
+var depths = new List<int>();
+
+// Read the contents of the file into a List<int>. Since we
+// know that all of the values will be `int` types, we can use
+// `int.Parse/1` instead of `int.TryParse/2`.
+foreach (string line in File.ReadLines("input.txt"))
+{
+    depths.Add(int.Parse(line));
+}
+
+int depthIncreaseCount = 0;
+
+// Loop over all of the values, skipping the first value
+// since there is not a previous value to compare it to.
+// My thinking of skipping the first value and checking
+// the index behind the current value we is to avoid having
+// to do a bounds check at the end of the array.
+for (int i = 0; i < depths.Count; i++)
+{
+    if (i > 0)
+    {
+        if (depths[i - 1] < depths[i])
+        {
+            depthIncreaseCount++;
+        }
+    }
+}
+
+Console.WriteLine("--- Day 1: Part One ---");
+Console.WriteLine($"Depth increased {depthIncreaseCount} times");
+
+// Part Two
+
+int groupDepthIncreaseCount = 0;
+int? previousGroup = null;
+
+// Loop over all of the values, skipping the first two, so that we can
+// sum the three values next to each other. My thinking on skipping the
+// first two values is that we can avoid doing a bounds check at the 
+// end of the array and just let the loop run to completion.
+for (int i = 0; i < depths.Count; i++)
+{
+    if (i > 1)
+    {
+        int currentGroup = depths[i - 2] + depths[i - 1] + depths[i];
+
+        if (currentGroup > previousGroup)
+        {
+            groupDepthIncreaseCount++;
+        }
+
+        previousGroup = currentGroup;
+    }
+}
+
+Console.WriteLine("--- Day 1: Part Two ---");
+Console.WriteLine($"Grouped depth increased {groupDepthIncreaseCount} times");
---
+++ AdventOfCode.Day02/Program.cs
@@ -0,0 +1,73 @@
+﻿// Part One
+
+const string DirectionForward = "forward";
+const string DirectionUp = "up";
+const string DirectionDown = "down";
+
+var instructions = new List<Instruction>();
+
+// Read all of the lines from the input.txt file into a 
+// List<Instruction> collection while parsing the values
+// out of each string/line.
+foreach (string line in File.ReadLines("input.txt"))
+{
+    var spaceIndex = line.IndexOf(' ');
+    string direction = line[..(spaceIndex)];
+    int moves = int.Parse(line[(spaceIndex)..]);
+    
+    instructions.Add(new Instruction(direction, moves));
+}
+
+int horizontalPosition = 0;
+int verticalPosition = 0;
+
+foreach ((string direction, int moves) in instructions)
+{
+    switch (direction)
+    {
+        case DirectionForward:
+            horizontalPosition += moves;
+            break;
+        case DirectionUp:
+            verticalPosition -= moves;
+            break;
+        case DirectionDown:
+            verticalPosition += moves;
+            break;
+    }
+}
+
+Console.WriteLine("--- Day 2: Part One ---");
+Console.WriteLine($"Horizontal position: {horizontalPosition}");
+Console.WriteLine($"Vertical position: {verticalPosition}");
+Console.WriteLine($"Horizontal * Vertical = {horizontalPosition * verticalPosition}");
+
+// Part Two
+
+horizontalPosition = 0;
+verticalPosition = 0;
+int aim = 0;
+
+foreach ((string direction, int moves) in instructions)
+{
+    switch (direction)
+    {
+        case DirectionForward:
+            horizontalPosition += moves;
+            verticalPosition += aim * moves;
+            break;
+        case DirectionUp:
+            aim -= moves;
+            break;
+        case DirectionDown:
+            aim += moves;
+            break;
+    }
+}
+
+Console.WriteLine("--- Day 2: Part Two ---");
+Console.WriteLine($"Horizontal position: {horizontalPosition}");
+Console.WriteLine($"Vertical position: {verticalPosition}");
+Console.WriteLine($"Horizontal * Vertical = {horizontalPosition * verticalPosition}");
+
+internal record Instruction(string Direction, int Moves);
